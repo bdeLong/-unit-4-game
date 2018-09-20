c41ecf9894b7b1fd72bf09d05fcc46ef1d529c5d
Ben deLong <bendelong45@gmail.com>
2018-09-20 18:56:35 -0500

    more tweaks

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,5 @@
 //initialize variables for game
 
-var guessesLeft = 10;
 var wins = 0;
 var wrestlers = ["Goldust", "Matt Hardy", "The Miz", "Honky Tonk Man", "Beth Phoenix", "Bam Bam Bigelow", "Rockin Robin", "Lex Luger", "Goldberg", "Mickie James", "Kofi Kingston", "Trish Stratus", "The Dudley Boys", "Sid Vicious", "Lita", "Batista", "Chyna", "The Iron Sheik", "Legion of Doom", "Chris Benoit", "Mr Fuji", "Yokozuna", "CM Punk", "Booker T", "Sgt Slaughter", "Big Show", "John Bradshaw Layfield", "Razor Ramon", "Diesel", "Rey Mysterio", "Owen Hart", "Jeff Hardy", "Randy Orton", "The Ultimate Warrior", "Brock Lesnar", "Wendi Richter", "Kane", "Ric Flair", "Rowdy Roddy Piper", "Triple H", "Chris Jericho", "Edge", "Mick Foley", "Eddie Guerrero", "John Cena", "Ted DiBiase", "Kurt Angle", "Macho Man Randy Savage", "The Rock", "Bret Hart", "Andre The Giant", "The Undertaker", "Stone Cold Steve Austin", "Shawn Michaels", "Hulk Hogan"]
 
@@ -25,14 +24,14 @@ startGame.onclick = function () {
   winLossStory.innerHTML = "";
   startGame.blur();
   startGame.textContent = "Reset Game";
-  guessesLeft = 12;
+  guessesLeft = 10;
   guessCount.textContent = guessesLeft;
   winCount.textContent = wins;
   guessedLettersContainer.textContent = guessedLetters;
 
   // grabs Wrestler from array and sets that as the word to be guessed
   var grabWord = wrestlers[Math.floor(Math.random() * wrestlers.length)].toUpperCase();
-  // Takes grabbed Wrestler name and converts it to underscores and dashes
+  // Takes grabbed Wrestler name and converts it to underscores and spaces
   var hiddenWord = [];
   for (var i = 0; i < grabWord.length; i++) {
     if (grabWord[i] === " ") {
@@ -50,18 +49,19 @@ startGame.onclick = function () {
   document.onkeyup = function (event) {
     var userGuess = event.key.toUpperCase();
     // checks if userGuess is not in word or guessed already.  decremnts guessesLeft by 1.
-    if (grabWord.indexOf(userGuess) === -1 && guessedLetters.indexOf(userGuess)=== -1 && guessesLeft>0 && hiddenWord.join('') !== grabWord){
+    if (grabWord.indexOf(userGuess) === -1 && guessedLetters.indexOf(userGuess) === -1 && guessesLeft > 0 && hiddenWord.join('') !== grabWord && validInputs.indexOf(userGuess) < 52) {
       guessesLeft--;
     }
     // pushes userGuess to guessedLetters array if it's a new letter
-    if (guessedLetters.indexOf(userGuess) === -1 && validInputs.indexOf(userGuess) > -1 && validInputs.indexOf(userGuess) < 52 && guessesLeft > 0 && hiddenWord.join('') !== grabWord){
+    if (guessedLetters.indexOf(userGuess) === -1 && validInputs.indexOf(userGuess) > -1 && validInputs.indexOf(userGuess) < 52 && guessesLeft > 0 && hiddenWord.join('') !== grabWord) {
       guessedLetters.push(userGuess);
       guessedLettersContainer.textContent = guessedLetters.join(",\xa0 ");
     }
-// if user hits a key that is not a letter alerts them to pick a letter
-    if (validInputs.indexOf(userGuess) === -1 && userGuess ) {
+    // if user hits a key that is not a letter alerts them to pick a letter
+    if (validInputs.indexOf(userGuess) === -1 && userGuess) {
       alert("Please press a letter");
     }
+    // if user guesses a letter in the word, replace underscore with correct letter
     for (var i = 0; i < grabWord.length; i++) {
       if (grabWord[i] === " ") {
         hiddenWord[i] = " ";
@@ -72,15 +72,15 @@ startGame.onclick = function () {
         guessedLettersContainer.textContent = guessedLetters.join(",\xa0 ");
       }
     }
-
-    if (hiddenWord.join('') === grabWord){
+    // once hidden word is revealed says you win and updates win count
+    if (hiddenWord.join('') === grabWord) {
       winLossMessage.textContent = "YOU WIN!";
       wins++;
       winCount.textContent = wins;
     }
-
+    // if user loses all guesses left, says you lose
     guessCount.textContent = guessesLeft;
-    if (guessesLeft === 0){
+    if (guessesLeft === 0) {
       winLossScreen.innerHTML = "<img src=\"Assets/images/HellInACell.gif\">";
       winLossMessage.textContent = "YOU LOSE!";
       winLossStory.textContent = "OOF! That was painful .... But not as painful as Mankind plummeting 16 ft through an announcer's table when The Undertaker threw him off Hell IN A Cell in 1998."
